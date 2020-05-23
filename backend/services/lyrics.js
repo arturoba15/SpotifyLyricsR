4738a3d74a881f84da03b53582ecab0335c47c60
Arturo Balbuena <arturo-balbuena@hotmail.com>
2020-05-22 22:52:52 -0500

    Now lyrics inside script tags are displayed

--- backend/services/lyrics.js
+++ backend/services/lyrics.js
@@ -32,11 +32,17 @@ const getLyrics = async url =>
   axios.get(url)
     .then(res => {
       const $ = cheerio.load(res.data);
-      const lyrics = $('.lyrics p').text(); 
-      // Sometimes the lyrics are inside <meta> tags for some reason
-      if (lyrics === '') throw createError(422, 'Couldn\'nt find lyrics in HTML');
+      let lyrics = $('.lyrics p').text(); 
+      // Sometimes lyrics are inside a <script> tag
+      if (lyrics === '') {
+        const scriptTag = $('body > script:not([src])').html();
+        const parsedJson = eval(scriptTag.substring(36, scriptTag.indexOf('window.__APP')));
+        const lyricsObj = parsedJson.songPage.lyricsData.body;
+        lyrics = lyricsObj.children[0].children.filter(e => typeof e === 'string').join('\n');
+      }
       return lyrics;
-    });
+    })
+    .catch(res => {throw createError(422, 'Couldn\'nt find lyrics in HTML')});
 
 exports.getBestHit = getBestHit;
 exports.getLyrics = getLyrics;
