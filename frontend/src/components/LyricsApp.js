ce8cb1558b705437475cbacb7041736df4f1eab9
Arturo Balbuena <arturo-balbuena@hotmail.com>
2020-05-24 13:06:42 -0500

    Fixed height of lyrics panel

--- frontend/src/components/LyricsApp.js
+++ frontend/src/components/LyricsApp.js
@@ -30,6 +30,14 @@ class LyricsApp extends React.Component {
           <LyricsPanel lyrics={song.lyrics}/>
         </div>
       );
+    } else if (this.state.error) {
+      const error = this.state.error;
+      return (
+        <div className="LyricsApp">
+          <SongPanel error={error}/>
+          <LyricsPanel lyrics={song.lyrics}/>
+        </div>
+      );
     } else {
       return (
         <div>
--- frontend/src/components/SongPanel.js
+++ frontend/src/components/SongPanel.js
@@ -5,7 +5,7 @@ class SongPanel extends React.Component {
     return (
       <div className="song panel">
         <img src={this.props.song.img} alt="Album image" />
-        <h1>{this.props.song.title}</h1>
+        <h1>{this.props.song.title || this.props.error}</h1>
         <h2>{this.props.song.artist}</h2>
       </div>
     );
