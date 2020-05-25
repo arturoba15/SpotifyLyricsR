6c1f38a021b4ec822390fbc575e08706f60e70d9
Arturo Balbuena <arturo-balbuena@hotmail.com>
2020-05-25 16:24:35 -0500

    Added initial screen with login button

--- frontend/src/components/LyricsApp.js
+++ frontend/src/components/LyricsApp.js
@@ -5,20 +5,28 @@ import LyricsPanel from './LyricsPanel';
 import SongPlaceholder from './SongPlaceholder';
 import LyricsPlaceholder from './LyricsPlaceholder';
 import ErrorPanel from './ErrorPanel';
+import LoginPanel from './LoginPanel';
 
 class LyricsApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      song: {}
+      song: {},
+      data: false,
+      loading: true
     }
   }
   
   componentDidMount() {
+    this.setState({ loading: true });
     axios.get('/lyrics')    
       .then(res => {
-        const song = res.data;
-        this.setState({ song });
+        if (res.data) {
+          const song = res.data;
+          this.setState({ song, data: true, loading: false });
+        } else {
+          this.setState({ data: false, loading: false });
+        }
       })
       .catch(err => {
         const errorMsg = err.response.data.message;
@@ -26,35 +34,56 @@ class LyricsApp extends React.Component {
           song: {
           ...prev.song,
           error: errorMsg
-          }
+          },
+          data: true,
+          loading: false
         }));
       });
   }
 
   render() {
-    const song = this.state.song;
-    if (song.title) {
-      return (
-        <div className="LyricsApp">
-          <SongPanel song={song}/>
-          <LyricsPanel lyrics={song.lyrics}/>
-        </div>
-      );
-    } else if (song.error) {
-      return (
-        <div className="LyricsApp">
-          <ErrorPanel error={song.error} />
-          <LyricsPanel />
-        </div>
-      );
+    if (this.state.loading) {
+        return (
+          <div>
+            <SongPlaceholder />
+            <LyricsPlaceholder />
+          </div>
+        );
     } else {
-      return (
-        <div>
-          <SongPlaceholder />
-          <LyricsPlaceholder />
-        </div>
-      );
+      if (this.state.data) {
+        const song = this.state.song;
+        if (song.title) {
+          return (
+            <div className="LyricsApp">
+              <SongPanel song={song}/>
+              <LyricsPanel lyrics={song.lyrics}/>
+            </div>
+          );
+        } else if (song.error) {
+          return (
+            <div className="LyricsApp">
+              <ErrorPanel error={song.error} />
+              <LyricsPanel />
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <SongPlaceholder />
+              <LyricsPlaceholder />
+            </div>
+          );
+        }
+      } else {
+          return (
+            <div className="LyricsApp">
+              <LoginPanel />
+              <LyricsPlaceholder />
+            </div>
+          );
+      }
     }
+
   }
 };
 
